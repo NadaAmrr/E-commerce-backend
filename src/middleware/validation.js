3a85350f62046ff92a5d566ff19cee43e40f4215
NadaAmrr <nada.amr1267@gmail.com>
2023-12-19 02:34:42 +0200

    setup finish

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,9 +1,12 @@
 import joi from "joi";
 import { Types } from "mongoose";
 
+const roles = {
+  admin: "admin",
+  customer: "customer",
+};
+
 const validateObjectId = (value, helper) => {
-  console.log({ value });
-  console.log({ helper });
   return Types.ObjectId.isValid(value)
     ? true
     : helper.message("In-valid objectId");
@@ -19,7 +22,7 @@ export const generalFields = {
     .required(),
   password: joi
     .string()
-    .pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/))
+    // .pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/))
     .required(),
   cPassword: joi.string().required(),
   id: joi.string().custom(validateObjectId).required(),
@@ -33,6 +36,13 @@ export const generalFields = {
     originalname: joi.string().required(),
     fieldname: joi.string().required(),
   }),
+  ln: joi.string().valid("en", "ar").required().messages({
+    "any.required": "Language is required.",
+    "string.base": "Language must be a string.",
+    "any.only": 'Language must be either "en" or "ar".',
+  }),
+  role: joi.valid(roles.admin || roles.customer).required(),
+  code: joi.string().regex(/^\d{5}$/),
 };
 
 export const validation = (schema) => {
@@ -41,7 +51,6 @@ export const validation = (schema) => {
     if (req.file || req.files) {
       inputData.file = req.file || req.files;
     }
-    console.log({ inputData });
     const result = schema.validate(inputData, { abortEarly: false });
     if (result.error?.details) {
       return res.status(400).json({
